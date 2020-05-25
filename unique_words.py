841377b28f9e33ff59d3e767e5e62da3fedf65fa
JLS <jonathan.sievers@gmail.com>
2020-05-25 23:13:06 +0200

    Added some comments

--- unique_words.py
+++ unique_words.py
@@ -4,16 +4,16 @@ def read_file(fname='state_names.txt'):
     f=open(fname)
     lines=f.readlines()
     f.close()
-    iref=ord('a')
+    iref=ord('a') #we'll want to convert characters to their 0-25 ordinal value, so subtract ord('a') to get there
     n=len(lines)
     lets=np.zeros([n,26])
     lens=np.zeros(n)
-    for i in range(n):
+    for i in range(n): #for each word, make a non-zero entry for every letter it contains
         lines[i]=lines[i].strip()
         ll=lines[i].lower()
         for j in range(len(ll)):
             ind=ord(ll[j])-iref
-            if (ind>=0)&(ind<26):
+            if (ind>=0)&(ind<26):  #skip anything that's not a letter (e.g. spaces in state names)
                 lets[i][ind]=1
         lens[i]=len(ll)
     return lets,lens,lines
@@ -24,15 +24,15 @@ overlap=np.dot(words,states.T) #this is an nstate by nword matrix that will be z
 overlap=0+overlap>0 #replace the matrix product by a boolean array that's true iff there's letters in common
 isunique=np.sum(overlap,axis=1)==overlap.shape[1]-1 #find all rows where only a single element is false
 isunique=np.where(isunique)[0]
-lens_unique=word_lens[isunique]
-inds=np.where(lens_unique==lens_unique.max())[0]
-for ind in inds:
+lens_unique=word_lens[isunique]  #this is the length of every mackerel
+inds=np.where(lens_unique==lens_unique.max())[0] #find the index of all words matching the longest mackerel
+for ind in inds:  #and print it out, with the corresponding state
     ii=isunique[ind]
     jj=np.where(overlap[ii,:]==False)[0][0]
     print(word_names[ii]+' shares no letters with ' + state_names[jj])
 
 tmp=overlap[isunique,:] #pull the entries out of the matrix that have only one state
-n_by_state=np.sum(tmp==False,axis=0)
+n_by_state=np.sum(tmp==False,axis=0)  #and find how many belong to each state
 for i in range(len(state_names)):
     if n_by_state[i]>0:
         print(state_names[i]+' has ' + repr(n_by_state[i]) + ' mackerels.')
